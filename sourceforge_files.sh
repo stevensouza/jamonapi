032ca848df31dbce0ba154f7d19fcd64c80937b1
Steve Souza <steve@stevesouza.com>
2014-07-05 17:31:54 +0200

    removed comment

--- sourceforge_files.sh
+++ sourceforge_files.sh
@@ -1,4 +1,4 @@
-# mkdir Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.77/
+mkdir Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.77/
 cp ./jamon/target/*.jar /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.77/.
 cp ./jamon_osmon/target/*.jar  /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.77/.
 cp ./jamon_war/target/*.war  /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.77/.
