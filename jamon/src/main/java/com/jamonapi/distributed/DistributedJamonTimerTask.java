d1745e3434acb995bdf1947d26462d146cea21c6
Steve Souza <steve@stevesouza.com>
2014-07-13 08:41:18 +0200

    added comments

--- jamon/src/main/java/com/jamonapi/distributed/DistributedJamonTimerTask.java
+++ jamon/src/main/java/com/jamonapi/distributed/DistributedJamonTimerTask.java
@@ -4,6 +4,10 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 /**
+ * Task that starts a thread that will call the JamonData put method on a timer.  The put method persists jamon data
+ * (MonitorComposite which is serializable).  The JamonServletContextListener automatically starts this thread for
+ * web applications.
+ *
  * Created by stevesouza on 7/7/14.
  */
 public class DistributedJamonTimerTask extends TimerTask {
@@ -17,6 +21,11 @@ public class DistributedJamonTimerTask extends TimerTask {
         jamonData.put();
     }
 
+    /** Start thread that will save jamon data (MonitorComposite). 
+     *
+     * @param refreshRateInMs frequency save should be executed.
+     * @return The scheduled timer.
+     */
     public Timer schedule(int refreshRateInMs) {
         Timer timer = new Timer(DistributedJamonTimerTask.class.getSimpleName()+"-saveJamonData");
         // use refreshRate for 1st value:  when to start, and how long to wait until next one.
