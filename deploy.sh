5c2e82a82fab6f13943257f67aeb21d9d6ca580e
Steve Souza <steve@stevesouza.com>
2014-07-26 16:33:48 +0200

    copy compiled code to vagrant too

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 cp jamon/target/jamon-2.79.jar /Applications/myapps/jetty/jetty-distribution-9.2.1.v20140609/lib/ext/.
+cp jamon/target/jamon-2.79.jar /Applications/myapps/vagrant/dev1/jetty-distribution-9.2.1.v20140609/lib/ext/.
 cp jamon/target/jamon-2.79.jar /Applications/myapps/tomcat/apache-tomcat-8.0.8/lib/.
+
 cp jamon_war/target/jamon.war /Applications/myapps/jetty/jetty-distribution-9.2.1.v20140609/webapps/.
+cp jamon_war/target/jamon.war /Applications/myapps/vagrant/dev1/jetty-distribution-9.2.1.v20140609/webapps/.
 cp jamon_war/target/jamon.war /Applications/myapps/tomcat/apache-tomcat-8.0.8/webapps/.
