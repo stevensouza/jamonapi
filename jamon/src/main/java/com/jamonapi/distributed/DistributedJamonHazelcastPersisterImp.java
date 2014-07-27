e4496f16115035b3bc6d31d8c1b53e0615b788f8
Steve Souza <steve@stevesouza.com>
2014-07-27 14:24:04 +0200

    Removed unnecessary code that saved server key as a seperate map

--- jamon/src/main/java/com/jamonapi/distributed/DistributedJamonHazelcastPersisterImp.java
+++ jamon/src/main/java/com/jamonapi/distributed/DistributedJamonHazelcastPersisterImp.java
@@ -8,6 +8,7 @@ import com.jamonapi.MonitorFactory;
 
 import java.util.Date;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**  Class that interacts with HazelCast to save jamon data to it so data from any jvms in the hazelcast cluster
  * can be visible via the jamon web app.  Note in must cases hazelcast exceptions are not bubbled up in this class
@@ -21,8 +22,6 @@ public class DistributedJamonHazelcastPersisterImp implements JamonDataPersister
 
     // could be Map if we don't want the instance methods of hazelcast
     private IMap<String, MonitorComposite> jamonDataMap;
-    // This should really be an ISet, but ISet doesn't support time-to-live methods
-    private IMap<String, Date> instances;
     private HazelcastInstance hazelCast;
     private LocalJamonDataPersister localJamonData = new LocalJamonDataPersister();
 
@@ -38,7 +37,7 @@ public class DistributedJamonHazelcastPersisterImp implements JamonDataPersister
     public Set<String> getInstances() {
       // I don't ever want to not display data when there is a hazelcast error.
       intitialize();
-      return instances.keySet();
+      return new TreeSet<String>(jamonDataMap.keySet());
     }
 
     @Override
@@ -51,22 +50,18 @@ public class DistributedJamonHazelcastPersisterImp implements JamonDataPersister
     public void put(String key) {
         intitialize();
         jamonDataMap.set(key, MonitorFactory.getRootMonitor().setInstanceName(key));
-        instances.set(key, new Date());
     }
 
 
     @Override
     public MonitorComposite get(String key) {
        intitialize();
-       // done purely to ensure instance and data live the same amount of time.
-       instances.get(key);
        return  jamonDataMap.get(key);
     }
 
     @Override
     public void remove(String instanceKey) {
        intitialize();
-       instances.remove(instanceKey);
        jamonDataMap.remove(instanceKey);
     }
 
@@ -87,7 +82,6 @@ public class DistributedJamonHazelcastPersisterImp implements JamonDataPersister
     private void intitialize() {
         if (jamonDataMap == null) {
             jamonDataMap = hazelCast.getMap(MonitorComposite.class.getCanonicalName());
-            instances = hazelCast.getMap("com.jamonapi.instances");
         }
     }
 
