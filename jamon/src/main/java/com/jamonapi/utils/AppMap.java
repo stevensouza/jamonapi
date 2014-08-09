ecc43c14e40575266791dbde9bbaf7b289716433
Steve Souza <steve@stevesouza.com>
2014-08-09 10:16:11 +0200

    fixed case insensitive map to work with jdk 8.

--- jamon/src/main/java/com/jamonapi/utils/AppMap.java
+++ jamon/src/main/java/com/jamonapi/utils/AppMap.java
@@ -1,4 +1,5 @@
 package com.jamonapi.utils;
+import java.util.Iterator;
 import java.util.Map;
 
 
@@ -12,11 +13,8 @@ import java.util.Map;
  * Other than that this class works like a regular HashMap.
  */
 public class AppMap extends java.util.HashMap {
-
     private static final long serialVersionUID = 278L;
-
     public AppMap() {
-
     }
     /** Constructs an empty HashMap with the default initial capacity (16) and the default load factor (0.75).
      * 
@@ -26,7 +24,6 @@ public class AppMap extends java.util.HashMap {
         super(initialCapacity);
     }
 
-
     /** Constructs an empty HashMap with the specified initial capacity and the default load factor (0.75). */
     public AppMap(int initialCapacity, float loadFactor) {
         super(initialCapacity,loadFactor);
@@ -34,7 +31,23 @@ public class AppMap extends java.util.HashMap {
 
     /** Constructs an empty HashMap with the specified initial capacity and load factor. */
     public AppMap(Map m) {
-        putAll(m);
+       putAll(m);
+    }
+
+    /**
+     * Note up to jdk 8 putAll(m) called the put(...) method below similar to the implementation below.
+     * jdk8 must have changed putAll(m) to not call put.  This made it so the keys weren't converted to
+     * be case insensitive breaking tests.  Hence the following code was added to make this implementation explicit.
+     * @since 2.79
+     * @param m
+     */
+    @Override
+    public void putAll(Map m) {
+        Iterator<Map.Entry> iter = m.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry entry = iter.next();
+            put(entry.getKey(), entry.getValue());
+        }
     }
 
     @Override
