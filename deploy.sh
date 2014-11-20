c2f145ec21db5feb9a22fc9b37e9a16fe298e4b9
Steve Souza <steve@stevesouza.com>
2014-11-20 11:02:31 +0100

    Changed version to 2.80 - jmx

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-cp jamon/target/jamon-2.79.jar /Applications/myapps/jetty/jetty-distribution-9.2.1.v20140609/lib/ext/.
-cp jamon/target/jamon-2.79.jar /Applications/myapps/vagrant/dev1/jetty-distribution-9.2.1.v20140609/lib/ext/.
-cp jamon/target/jamon-2.79.jar /Applications/myapps/tomcat/apache-tomcat-8.0.8/lib/.
+cp jamon/target/jamon-2.80.jar /Applications/myapps/jetty/jetty-distribution-9.2.1.v20140609/lib/ext/.
+cp jamon/target/jamon-2.80.jar /Applications/myapps/vagrant/dev1/jetty-distribution-9.2.1.v20140609/lib/ext/.
+cp jamon/target/jamon-2.80.jar /Applications/myapps/tomcat/apache-tomcat-8.0.8/lib/.
 
 cp jamon_war/target/jamon.war /Applications/myapps/jetty/jetty-distribution-9.2.1.v20140609/webapps/.
 cp jamon_war/target/jamon.war /Applications/myapps/vagrant/dev1/jetty-distribution-9.2.1.v20140609/webapps/.
--- sourceforge_files.sh
+++ sourceforge_files.sh
@@ -1,6 +1,6 @@
-mkdir /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.79/
-cp ./jamon/target/*.jar /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.79/.
-cp ./jamon_osmon/target/*.jar  /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.79/.
-cp ./jamon_war/target/*.war  /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.79/.
+mkdir /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.80/
+cp ./jamon/target/*.jar /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.80/.
+cp ./jamon_osmon/target/*.jar  /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.80/.
+cp ./jamon_war/target/*.war  /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.80/.
 # note only the destination file name has to change.
-cp /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.76/jamontomcat-2.76.jar /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.79/jamontomcat-2.79.jar
+cp /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.76/jamontomcat-2.76.jar /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.80/jamontomcat-2.80.jar
