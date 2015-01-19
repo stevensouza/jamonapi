e3d87b5aef159ebfb37b6c21574e2d699d9b4cd8
Steve Souza <steve@stevesouza.com>
2015-01-19 21:14:04 +0100

    Changed version to jamon 2.81-SNAPSHOT

--- sourceforge_files.sh
+++ sourceforge_files.sh
@@ -1,6 +1,7 @@
-mkdir /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.80/
-cp ./jamon/target/*.jar /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.80/.
-cp ./jamon_osmon/target/*.jar  /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.80/.
-cp ./jamon_war/target/*.war  /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.80/.
+#!/bin/sh
+mkdir /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.81-SNAPSHOT/
+cp ./jamon/target/*.jar /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.81-SNAPSHOT/.
+cp ./jamon_osmon/target/*.jar  /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.81-SNAPSHOT/.
+cp ./jamon_war/target/*.war  /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.81-SNAPSHOT/.
 # note only the destination file name has to change.
-cp /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.76/jamontomcat-2.76.jar /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.80/jamontomcat-2.80.jar
+cp /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.76/jamontomcat-2.76.jar /Users/stevesouza/Documents/sourceforge/jamonrelease/jamonall-2.81-SNAPSHOT/jamontomcat-2.81-SNAPSHOT.jar
