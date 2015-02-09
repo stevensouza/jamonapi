7adb471d8fb2eeb98ec22e1411ab589b62f36200
Steve Souza <steve@stevesouza.com>
2015-02-09 11:22:37 +0100

    added deploy to tomcat7

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@
 cp jamon/target/jamon-2.81-SNAPSHOT.jar /Applications/myapps/jetty/jetty-distribution-9.2.1.v20140609/lib/ext/.
 cp jamon/target/jamon-2.81-SNAPSHOT.jar /Applications/myapps/vagrant/dev1/jetty-distribution-9.2.1.v20140609/lib/ext/.
 cp jamon/target/jamon-2.81-SNAPSHOT.jar /Applications/myapps/tomcat/apache-tomcat-8.0.8/lib/.
+cp jamon/target/jamon-2.81-SNAPSHOT.jar /Applications/myapps/tomcat/apache-tomcat-7.0.54/lib/.
 
 cp jamon_war/target/jamon.war /Applications/myapps/jetty/jetty-distribution-9.2.1.v20140609/webapps/.
 cp jamon_war/target/jamon.war /Applications/myapps/vagrant/dev1/jetty-distribution-9.2.1.v20140609/webapps/.
 cp jamon_war/target/jamon.war /Applications/myapps/tomcat/apache-tomcat-8.0.8/webapps/.
+cp jamon_war/target/jamon.war /Applications/myapps/tomcat/apache-tomcat-7.0.54/webapps/.
