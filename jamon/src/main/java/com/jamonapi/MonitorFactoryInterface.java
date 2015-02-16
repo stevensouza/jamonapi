eac19a1ffd0f62a2d11973da4dc1657cd4c2f253
Steve Souza <steve@stevesouza.com>
2015-02-16 18:14:05 +0100

    Changed version to 2.82

--- jamon/src/main/java/com/jamonapi/MonitorFactoryInterface.java
+++ jamon/src/main/java/com/jamonapi/MonitorFactoryInterface.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 public interface MonitorFactoryInterface extends Serializable {
 
-    public static final String VERSION="2.81";
+    public static final String VERSION="2.82";
 
     /** Return a monitor with the given label and units.  Note label has an effect on what range is used.  If no range is
      * associated with units then it will use the null range (i.e. no range)
