4a3d1d754c30f1e7167999b553f98e716f025891
Steve Souza <steve@stevesouza.com>
2015-02-16 12:28:37 +0100

    httpStatus XX - added jmx http status unit tests

---
+++ jamon/src/test/java/com/jamonapi/jmx/HttpStatusMXBeanImpTest.java
@@ -0,0 +1,58 @@
+package com.jamonapi.jmx;
+
+import com.jamonapi.MonitorFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HttpStatusMXBeanImpTest {
+    private HttpStatusMXBean bean = new HttpStatusMXBeanImp();
+
+    @Before
+    public void setUp() throws Exception {
+        MonitorFactory.reset();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        MonitorFactory.reset();
+    }
+
+    @Test
+    public void testGet1xx() throws Exception {
+        createStatusCodeMonitors(1);
+        assertThat(bean.get1xx()).isEqualTo(1);
+    }
+
+    @Test
+    public void testGet2xx() throws Exception {
+        createStatusCodeMonitors(2);
+        assertThat(bean.get2xx()).isEqualTo(2);
+    }
+
+    @Test
+    public void testGet3xx() throws Exception {
+        createStatusCodeMonitors(3);
+        assertThat(bean.get3xx()).isEqualTo(3);
+    }
+
+    @Test
+    public void testGet4xx() throws Exception {
+        createStatusCodeMonitors(4);
+        assertThat(bean.get4xx()).isEqualTo(4);
+    }
+
+    @Test
+    public void testGet5xx() throws Exception {
+        createStatusCodeMonitors(5);
+        assertThat(bean.get5xx()).isEqualTo(5);
+    }
+
+    private void createStatusCodeMonitors(int statusCode) {
+        for (int i=0;i<statusCode;i++) {
+            MonitorFactory.add(HttpStatusMXBean.LABEL + statusCode + "xx", HttpStatusMXBean.UNITS, statusCode);
+        }
+    }
+}
